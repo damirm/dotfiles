3d7f40b05f67bd255632447eae23cb7ecee3141c
Damir Makhmutov <iam@damir.space>
2025-08-28 12:25:47 +0300

    Add flatpak packages

--- install-fedora-packages.sh
+++ install-fedora-packages.sh
@@ -13,3 +13,17 @@ if command -v gsettings &>/dev/null; then
     echo "Installing gnome settings..."
     gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_shifted_capslock']"
 fi
+
+echo "Installig flatpak packages..."
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak install flathub org.mozilla.Thunderbird
+# flatpak install flathub eu.betterbird.Betterbird
+# flatpak install flathub org.gnome.Extensions
+flatpak install flathub org.mozilla.firefox
+flatpak install flathub org.videolan.VLC
+flatpak install flathub org.telegram.desktop
+flatpak install flathub com.visualstudio.code
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub com.transmissionbt.Transmission
+flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
