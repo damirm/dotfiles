7e892dc1c01e87884e2dc88a1c3b19f8b944664b
Damir Makhmutov <iam@damir.space>
2025-08-30 00:55:23 +0300

    Fixed hotkeys

--- install-fedora-packages.sh
+++ install-fedora-packages.sh
@@ -20,28 +20,6 @@ function install_nerd_font {
     rm $tmp_dir/$zip_file
 }
 
-function install_gnome_extension {
-    local extension_id=$1
-    local remote_file=$2
-    local zip_file=$(basename $remote_file)
-    local tmp_dir="/tmp"
-    local dst_dir="$HOME/.local/share/gnome-shell/extensions/$extension_id"
-
-    if test -d $dst_dir; then
-        echo "Gnome extension $extension_id is already installed, skipping..."
-        return 0
-    fi
-
-    echo "Installing gnome extension $extension_url from $remote_file..."
-
-    wget -P $tmp_dir $remote_file
-    mkdir -p $dst_dir
-    # TODO: Move to temporary dir first, then extract uuid, and then move to extensions dir.
-    unzip -o $tmp_dir/$zip_file -d $dst_dir
-    gnome-extensions enable $extension_id
-    rm $tmp_dir/$zip_file
-}
-
 echo "Setup dnf..."
 sudo dnf config-manager setopt fastestmirror=true
 sudo dnf config-manager setopt max_parallel_downloads=10
@@ -52,7 +30,8 @@ if ! dnf repolist | grep -q rpmfusion; then
         https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
         https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-    sudo dnf update -y
+    sudo dnf update -y --skip-unavailable
+    # TODO: Do we need to update @core if we updated all above?
     sudo dnf update -y @core
 fi
 
@@ -92,7 +71,14 @@ sudo dnf install -y \
     jetbrains-mono-nl-fonts \
     1password \
     htop \
-    btop
+    btop \
+    pipx \
+    gum \
+    fd-find \
+    bat \
+    eza \
+    tldr \
+    ulauncher
 
 install_nerd_font "JetBrainsMono" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
 
@@ -102,23 +88,27 @@ if command -v gsettings &>/dev/null; then
     sudo dnf install -y \
         gnome-shell-extension-dash-to-dock
 
-    install_gnome_extension "appindicatorsupport@rgcjonas.gmail.com" "https://github.com/ubuntu/gnome-shell-extension-appindicator/releases/download/v60/appindicatorsupport@rgcjonas.gmail.com.zip"
-    install_gnome_extension "Vitals@CoreCoding.com" "https://github.com/corecoding/Vitals/releases/download/v72.0.0/vitals.zip"
-    # install_gnome_extension "space-bar@luchrioh" "https://github.com/christopher-l/space-bar/releases/download/v33/space-bar@luchrioh.shell-extension.zip"
+    pipx install gnome-extensions-cli
+
+    gext install space-bar@luchrioh
+    gext install appindicatorsupport@rgcjonas.gmail.com
+    gext install Vitals@CoreCoding.com
+    gext install just-perfection-desktop@just-perfection
+    gext install undecorate@sun.wxg@gmail.com
 
     echo "Installing gnome settings..."
     gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier', 'grp:alt_space_toggle']"
     gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'en'), ('xkb', 'ru')]"
-    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    gsettings set org.gnome.desktop.interface show-battery-percentage true
+
     gsettings set org.gnome.mutter dynamic-workspaces false
     gsettings set org.gnome.desktop.wm.preferences num-workspaces 5
 
-    gsettings reset org.gnome.desktop.wm.keybindings switch-to-application-1
-    gsettings reset org.gnome.desktop.wm.keybindings switch-to-application-2
-    gsettings reset org.gnome.desktop.wm.keybindings switch-to-application-3
-    gsettings reset org.gnome.desktop.wm.keybindings switch-to-application-4
-    gsettings reset org.gnome.desktop.wm.keybindings switch-to-application-5
+    gsettings set org.gnome.shell.keybindings switch-to-application-1 "['disabled']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-2 "['disabled']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-3 "['disabled']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-4 "['disabled']"
+    gsettings set org.gnome.shell.keybindings switch-to-application-5 "['disabled']"
+
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
@@ -129,8 +119,27 @@ if command -v gsettings &>/dev/null; then
     gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Shift><Super>3']"
     gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Shift><Super>4']"
     gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 "['<Shift><Super>5']"
-    gsettings set org.gnome.shell.keybindings toggle-application-view "['<Control>Space']"
+    gsettings reset org.gnome.shell.keybindings toggle-application-view
+    # gsettings set org.gnome.shell.keybindings toggle-application-view "['<Control>Space']"
+    gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>q']"
+    gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
+
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    gsettings set org.gnome.desktop.interface show-battery-percentage true
     gsettings set org.gnome.desktop.interface enable-animations false
+    gsettings set org.gnome.desktop.interface clock-format '24h'
+    gsettings set org.gnome.desktop.interface clock-show-date true
+    gsettings set org.gnome.desktop.calendar show-weekdate true
+    gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+    gsettings set org.gnome.mutter center-new-windows true
+    gsettings set org.gnome.mutter auto-maximize false
+
+    gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+
+    gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'ulauncher-toggle'
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'ulauncher-toggle'
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control>space'
 fi
 
 echo "Installing flatpak packages..."
