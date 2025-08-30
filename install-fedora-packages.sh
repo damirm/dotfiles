d573fee3d4553887cc7e59d9e71b1925ff05e75f
Damir Makhmutov <iam@damir.space>
2025-08-30 19:08:30 +0300

    Install byedpi

--- install-fedora-packages.sh
+++ install-fedora-packages.sh
@@ -81,7 +81,10 @@ sudo dnf install -y \
     tldr \
     ulauncher \
     pam-u2f \
-    pamu2fcfg
+    pamu2fcfg \
+    tor \
+    torsocks \
+    telnet
 
 install_nerd_font "JetBrainsMono" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
 
@@ -175,11 +178,13 @@ if command -v gsettings &>/dev/null; then
 fi
 
 # Setup yubikey
-if [ ! -f ~/.config/Yubico/u2f_keys ]; then
-    mkdir -p ~/.config/Yubico
-    echo "Touch yubikey: "
-    pamu2fcfg >~/.config/Yubico/u2f_keys
-    # TODO: configure /etc/pam.d/sudo, /etc/pam.d/gdm-password
+if ! lsusb | grep -i yubikey; then
+    if [ ! -f ~/.config/Yubico/u2f_keys ]; then
+        mkdir -p ~/.config/Yubico
+        echo "Touch yubikey: "
+        pamu2fcfg >~/.config/Yubico/u2f_keys
+        # TODO: configure /etc/pam.d/sudo, /etc/pam.d/gdm-password
+    fi
 fi
 
 echo "Installing flatpak packages..."
@@ -198,6 +203,22 @@ flatpak install -y flathub \
 
 # Misc settings.
 sudo systemctl disable NetworkManager-wait-online.service
+
+# Install byedpi
+if ! command -v ciadpi &>/dev/null; then
+    tmp_dir=$(mktemp -d)
+    remote_zip="https://github.com/hufrea/byedpi/releases/download/v0.17.2/byedpi-17.2-x86_64.tar.gz"
+    file_name=$(basename "$remote_zip")
+    wget -P "$tmp_dir" "$remote_zip"
+    tar zxvf "$tmp_dir/$file_name" -C "$tmp_dir"
+    mv "$tmp_dir/ciadpi-x86_64" "$HOME/.local/bin/ciadpi"
+    user_systemd_dir="$HOME/.config/systemd/user"
+    mkdir -p "$user_systemd_dir"
+    ln -s $(pwd)/fedora/home/.config/systemd/user/byedpi.service "$user_systemd_dir/byedpi.service"
+    systemctl --user daemon-reload
+    systemctl --user enable byedpi.service
+    systemctl --user start byedpi.service
+fi
 
 # TODO: Firefox plugins
 # TODO: Ulauncher settings
