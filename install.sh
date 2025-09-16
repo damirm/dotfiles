bee82ffcda01657d844d55359c1d4716cae506ca
Damir Makhmutov <iam@damir.space>
2025-09-16 11:00:21 +0300

    Add setup-foobar.sh scripts

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-if command -v brew &>/dev/null; then
-    echo "Installing brew packages..."
-    BREWFILE="$HOME/.config/homebrew/Brewfile"
-    brew bundle check -v --file $BREWFILE || brew bundle install --file $BREWFILE
+# TODO: Brewfile does not exist until stow is symlinked it.
+# It's required to install stow first, then symlink configs,
+# and only then install the rest of brew packages.
+if command -v brew &> /dev/null; then
+  echo "Installing brew packages..."
+  BREWFILE="$HOME/.config/homebrew/Brewfile"
+  brew bundle check -v --file $BREWFILE || brew bundle install --file $BREWFILE
 fi
 
-if command -v dnf &>/dev/null; then
-    ./install-fedora-packages.sh
+if command -v dnf &> /dev/null; then
+  ./install-fedora-packages.sh
 fi
 
 echo "Creating symlinks..."
---
+++ scripts/setup-macos.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# TODO:
+# - Remap caps_lock to left_control
