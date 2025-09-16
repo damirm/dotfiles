b05a4b78ef35bdb342de7d3e1376e3fbf9223208
Damir Makhmutov <iam@damir.space>
2025-09-16 22:53:02 +0300

    Install stow first

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# TODO: Brewfile does not exist until stow is symlinked it.
-# It's required to install stow first, then symlink configs,
-# and only then install the rest of brew packages.
 if command -v brew &> /dev/null; then
-  echo "Installing brew packages..."
-  BREWFILE="$HOME/.config/homebrew/Brewfile"
-  brew bundle check -v --file $BREWFILE || brew bundle install --file $BREWFILE
+  brew install stow
 fi
 
 if command -v dnf &> /dev/null; then
@@ -15,5 +10,11 @@ fi
 
 echo "Creating symlinks..."
 stow -t $HOME alacritty tmux nvim zsh brew starship kitty sesh mise git terraform scripts
+
+if command -v brew &> /dev/null; then
+  echo "Installing brew packages..."
+  BREWFILE="$HOME/.config/homebrew/Brewfile"
+  brew bundle check -v --file $BREWFILE || brew bundle install --file $BREWFILE
+fi
 
 ./install-packages.sh
