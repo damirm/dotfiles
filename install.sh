2b008be429049b77b9f3d6559ae256a13f346cb8
Damir Makhmutov <iam@damir.space>
2025-09-17 00:06:38 +0300

    Temporary disable some dotfiles

--- install.sh
+++ install.sh
@@ -5,31 +5,39 @@ if command -v brew &> /dev/null; then
 fi
 
 if command -v dnf &> /dev/null; then
+  echo "Configuring dnf..."
   ./setup/dnf.sh
+
+  echo "Configuring fedora..."
   ./setup/fedora.sh
 fi
 
 if command -v gsettings &> /dev/null; then
+  echo "Configuring gnome..."
   ./setup/gnome.sh
 fi
 
 if command -v flatpak &> /dev/null; then
+  echo "Configuring flatpak..."
   ./setup/flatpak.sh
 fi
 
 if command -v fc-cache &> /dev/null; then
+  echo "Configuring fonts..."
   ./setup/fonts.sh
 fi
 
-./setup/yubikey.sh
+if command -v lsusb &> /dev/null; then
+  ./setup/yubikey.sh
+fi
 
-echo "Creating symlinks..."
-stow -t $HOME alacritty tmux nvim zsh brew starship kitty sesh mise terraform scripts ssh git
+echo "Configuring dotfiles..."
+# TODO: Temporary disable scripts, ssh, git dotfiles.
+stow -t $HOME alacritty tmux nvim zsh brew starship kitty sesh mise terraform # scripts ssh git
 
 if command -v brew &> /dev/null; then
-  echo "Installing brew packages..."
-  BREWFILE="$HOME/.config/homebrew/Brewfile"
-  brew bundle check -v --file $BREWFILE || brew bundle install --file $BREWFILE
+  echo "Configuring homebrew..."
+  ./setup/brew.sh
 fi
 
 [[ $OSTYPE == 'darwin'* ]] && ./setup/macos.sh
