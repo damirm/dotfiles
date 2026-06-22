221a296af762a6c33224062d88d84d20cb902b19
Damir Makhmutov <iam@damir.space>
2026-06-22 23:20:29 +0300

    Sync configs with chezmoi version of dotfiles

--- chezmoi/dotfiles/.config/nvim/lua/plugins/colorscheme.lua
+++ chezmoi/dotfiles/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,22 @@
 return {
 	{
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		enabled = true,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("github-theme").setup({
+				-- ...
+			})
+
+			vim.cmd.colorscheme("github_dark_default")
+		end,
+	},
+	{
 		"mcauley-penney/techbase.nvim",
 		priority = 1000,
-		enabled = true,
+		enabled = false,
 		config = function()
 			vim.cmd.colorscheme("techbase")
 		end,
